1e0c1109a53d0028d23f5192cf889cd696ddb726
rohanop <rohanopdev@gmail.com>
2022-12-13 22:02:41 +0530

    Add apis

--- server/main.go
+++ server/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+	Done  bool   `json:"done"`
+}
 
 func main() {
-	fmt.Println("Hello World")
+	app := fiber.New()
+
+	todos := []Todo{}
+
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+		return c.JSON(todos)
+	})
+
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+		if err != nil {
+			return c.Status(401).SendString("Invalid id")
+		}
+
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		for i, t := range todos {
+			if t.ID == id {
+				todos[i] = *todo
+				break
+			}
+		}
+		return c.SendString("Updated successfully!")
+	})
+
+	app.Listen(":8080")
 }
